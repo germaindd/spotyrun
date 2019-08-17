47d785a3db3b0a0d73ba3079400309679749757e
Germain d'Udekem <germain.dudekem@tigerspike.com>
2019-08-17 23:04:51 +1000

    added comments and altered some functionality
    pretty terrible commit message i know

--- download_bpm.py
+++ download_bpm.py
@@ -1,11 +1,11 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import sys
 import subprocess
-import re
 
 credentials = SpotifyClientCredentials("70aca742c29e4bf0b10705f38d99faa3", "2a73c2a476044f7b82f6771b1ed31c63")
 sp = spotipy.Spotify(client_credentials_manager=credentials)
+OUTPUT_PATH = ''
+subprocess.run(['chmod', 'u+x', 'rubberband'])
 
 
 class Track:
@@ -22,30 +22,38 @@ class Track:
 
 
 def delete_originals(path: str):
-    subprocess.run(['rm', '{}/*.wav'.format(path)], shell=True)
+    """remove .wav files to keep only mp3s"""
+    subprocess.run('rm {}/*.wav'.format(path), shell=True)
 
 
 def convert_bpm(tracks: list, target_bpm):
     """convert the bpm of given tracks"""
-    subprocess.run(['mkdir', 'converted'])
+    subprocess.run(['mkdir', '{}/bpm_converted'.format(OUTPUT_PATH)])
     for track in tracks:
-        filename = "{}.wav".format(track.track_name.replace(',', ''))
+        # remove '.' in track name (spotdl does this so the os knows where the file extension is)
+        filename = "{}.wav".format(track.track_name.replace('.', '').replace('"', ''))
 
-        if track.bpm < 100:
-            track.bpm *= 2
+        # when i convert my running playlist I have bpm's around 80-90 and 160-180. I want both of these categories to
+        # be adjusted at the same factor otherwise the songs with bpm 80-90 are gonna end up going REALLY fast
+        base_bpm = track.bpm
+        if base_bpm < 100:
+            base_bpm *= 2
 
-        print("adjusting bpm {} to bpm {}\n".format(track, target_bpm))
-        subprocess.run(['rubberband', '--quiet', '--tempo', '{}:{}'.format(track.bpm, target_bpm),
-                        filename, 'converted/{}'.format(filename)])
-        subprocess.run(['ffmpeg', '-loglevel', 'level+error', '-i', 'converted/{}'.format(filename), 'converted/{}.mp3'.format(track.track_name)])
-        print('\n')
+        print("adjusting bpm {} to bpm {}".format(track, target_bpm))
+        subprocess.run(['./rubberband', '--quiet', '--tempo', '{}:{}'.format(base_bpm, target_bpm),
+                        "{}/{}".format(OUTPUT_PATH, filename), '{}/bpm_converted/{}'.format(OUTPUT_PATH, filename)])
+        print("converting to mp3")
+        subprocess.run(['ffmpeg', '-y', '-loglevel', 'level+error', '-i', '{}/bpm_converted/{}'.format(OUTPUT_PATH, filename),
+                        '{}/bpm_converted/{}.mp3'.format(OUTPUT_PATH, track.track_name)])
 
 
 def download_playlist(playlist_id, playlist_name: str):
     subprocess.run(['spotdl', '--playlist', playlist_id], stdout=subprocess.PIPE)
     subprocess.run(['spotdl', '--list', '{}.txt'.format(playlist_name.replace(' ', '-')), '--overwrite', 'force',
-                    '--file-format', '{track_name}', '--trim-silence', '--folder', '.', '--output-ext', '.wav'],
+                    '--file-format', '{track_name}', '--trim-silence', '--folder', OUTPUT_PATH, '--output-ext', '.wav'],
                    stdout=subprocess.PIPE)
+
+    print("\n------------------\nDownload finished\n------------------\n\n")
 
 
 def add_song_bpm(sp: spotipy.Spotify, tracks: list):
@@ -65,29 +73,43 @@ def get_playlist_tracks(playlist_id):
     for item in playlist_tracks['items']:
         id = item['track']['id']
         name = item['track']['name']
+
         tracks.append(Track(name, id))
 
+        # if there are multiple pages
         if playlist_tracks['next'] is not None:
             playlist_tracks = sp.get_playlist_tracks(playlist_id, offset=offset)
-            print(playlist_tracks)
     return tracks
 
 
 def main():
-    playlist_id = sys.argv[1].split(':')[-1]
-    target_bpm = sys.argv[2]
+    global OUTPUT_PATH
+
+    # get id from URI
+    playlist_id = input('enter spotify URI: ').split(':')[-1]
+    # get target bpm
+    target_bpm = input('enter target bpm: ')
+    OUTPUT_PATH = input('enter path of folder to output to (or press enter for current directory): ')
+
+    if OUTPUT_PATH == '':
+        OUTPUT_PATH = '.'
+    elif OUTPUT_PATH[-1] == '/':
+        OUTPUT_PATH = OUTPUT_PATH[:-1]
+
+    subprocess.run(['mkdir', OUTPUT_PATH])
 
     playlist_dict = sp.get_playlist(playlist_id)
     playlist_name = playlist_dict['name']
 
     tracks = get_playlist_tracks(playlist_id)
 
-    # add_song_bpm(sp, tracks)
-    #
-    # download_playlist(playlist_id, playlist_name)
-    #
-    # convert_bpm(tracks, target_bpm)
+    add_song_bpm(sp, tracks)
 
+    download_playlist(playlist_id, playlist_name)
+
+    convert_bpm(tracks, target_bpm)
+
+    delete_originals("{}/bpm_converted".format(OUTPUT_PATH))
 
 
 if __name__ == "__main__":
